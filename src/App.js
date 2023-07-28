7515bb6abc797e5d9cf1ca6e215698a5c472e396
Suzdong <bluerellsu@gmail.com>
2023-07-28 11:07:43 +1000

    Adding rechart
    npm rechart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 function App() {
   return (
@@ -20,6 +22,19 @@ function App() {
       </header>
     </div>
   );
+}
+
+function MyChart() {
+  return (
+    <LineChart width={600} height={300} data={data}>
+      <XAxis dataKey="name" />
+      <YAxis />
+      <CartesianGrid />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="value" stroke="#8884d8">
+    </LineChart>
+  )
 }
 
 export default App;
