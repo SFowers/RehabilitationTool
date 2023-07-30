3652a394b6769dd19c50364b275d2940e92f66cb
Chris Su <bluerellsu@gmail.com>
2023-07-30 18:54:24 +1000

    Use the component
    import the rehabilitationChart into file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import RehabilitationChart from './rehabilitationChart';
 
 function App() {
   return (
@@ -24,17 +25,27 @@ function App() {
   );
 }
 
-function MyChart() {
+const App = () => {
+  // Sample patient data (replace this with actual patient data)
+  const patientData = [
+    { dateTime: '2023-07-01', handWaves: 5, acrossTable: 8, raiseArm: 10 },
+    { dateTime: '2023-07-02', handWaves: 7, acrossTable: 9, raiseArm: 12 },
+    // Add more data points for each patient...
+  ];
+
+  // Sample normative data (replace this with actual normative data)
+  const normativeData = [
+    { dateTime: '2023-07-01', normative: 6 },
+    { dateTime: '2023-07-02', normative: 8 },
+    // Add more data points for each exercise type...
+  ];
+
   return (
-    <LineChart width={600} height={300} data={data}>
-      <XAxis dataKey="name" />
-      <YAxis />
-      <CartesianGrid />
-      <Tooltip />
-      <Legend />
-      <Line type="monotone" dataKey="value" stroke="#8884d8" />
-    </LineChart>
-  )
-}
+    <div>
+      <h1>Rehabilitation Tool</h1>
+      <rehabilitationChart patientData={patientData} normativeData={normativeData} />
+    </div>
+  );
+};
 
 export default App;
